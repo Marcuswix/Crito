93613b0c44dfc34a856b71756f2b4a6ab773ac06
Marcus <Marcuswix@yahoo.se>
2023-09-21 16:23:36 +0200

    All pages done in desktop and mobil version, index-page done in Ipad version

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // script.js
 const toggleButton = document.getElementById("toggle-menu");
 const menu = document.getElementById("menu");
+const overlay = document.getElementById("overlay");
 
 toggleButton.addEventListener("click", () => {
   if (menu.style.display === "none" || menu.style.display === "") {
     menu.style.display = "block";
+    overlay.style.display = "block";
   } else {
     menu.style.display = "none";
   }
 });
+
+overlay.addEventListener('click', () => {
+  menu.style.display = "none";
+  overlay.style.display = "none";
+})
